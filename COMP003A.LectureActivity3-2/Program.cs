b25af025d67f1a83165a7dea9bd4c4e7b1bf3c04
adodoCoder <alavropulido@whccd.edu>
2025-01-27 19:14:03 -0800

    Add project files.

---
+++ COMP003A.LectureActivity3-2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Author: [Alvaro Pulido]
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Demonstrate iterative statements in C#.
+namespace COMP003A.LectureActivity3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
